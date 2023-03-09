6bf3b86dd59416e526553d346f00fa0d8180186f
rbabaci1 <4rabah@gmail.com>
2023-03-08 17:28:30 -0800

    .

---
+++ server/schemas/index.js
@@ -0,0 +1,7 @@
+import user from './user'
+import post from './post'
+import comment from './comment'
+import postedBy from './postedBy'
+import save from './save'
+
+export const schemaTypes = [user, post, comment, postedBy, save]
